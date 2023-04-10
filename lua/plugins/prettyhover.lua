73a2484abd9df4de18fa220de8a23f81e789b552
caaallum <callum_speirs@protonmail.com>
2023-04-10 10:04:29 +0100

    Added pretyhover

---
+++ lua/plugins/prettyhover.lua
@@ -0,0 +1,34 @@
+local M = {
+  "Fildo7525/pretty_hover",
+  opts = function() 
+    return {
+      line = {
+        "@brief",
+        "\\brief",
+      },
+      word = {
+        "@param",
+        "@tparam",
+        "@see",
+        "\\param",
+        "\\tparam",
+        "\\see"
+      },
+      header = {
+        "@class",
+        "\\class"
+      },
+      stylers = {
+        line = "**",
+        word = "`",
+        header = "###"
+      },
+      border = "rounded"
+    }
+  end,
+  config = function(_, opts) 
+    require("pretty_hover").setup(opts)
+  end
+}
+
+return M
