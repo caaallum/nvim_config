d93a616006f630b17df09130513ffe4a86a7201f
caaallum <callum_speirs@protonmail.com>
2023-05-06 23:17:56 +0100

    Update autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,6 +1,7 @@
 local M = {
   "windwp/nvim-autopairs",
   commit = "7747bbae60074acf0b9e3a4c13950be7a2dff444",
+  event = "VeryLazy",
   config = function()
     require("nvim-autopairs").setup()
   end
