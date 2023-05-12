06249c066b256d309a986a9610a9f2b9101afe0a
caaallum <callum_speirs@protonmail.com>
2023-05-12 17:44:31 +0100

    Update colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,28 +3,24 @@ local M = {
   tag = "v1.1.1",
   name = "catppuccin",
   priority = 1000,
-  lazy = false,
-  opts = function()
-    return {
-      flavour = "mocha",
-      integrations = {
-        neotree = true,
-        treesitter = true,
-        ts_rainbow2 = true,
-        notify = true,
-        dashboard = true,
-        mini = true,
-        cmp = true
-      },
-      show_end_of_buffer = true,
-      term_colors = true
-    }
-  end
+  lazy = false
 }
 
 function M.config()
-  local catpuccin = require("catppuccin")
-  catpuccin.setup(opts)
+  require("catppuccin").setup({
+    flavour = "mocha",
+    integrations = {
+      neotree = true,
+      treesitter = true,
+      ts_rainbow2 = true,
+      notify = true,
+      dashboard = true,
+      mini = true,
+      cmp = true
+    },
+    show_end_of_buffer = true,
+    term_colors = true
+  })
 
   vim.cmd.colorscheme "catppuccin"
 end
