4640044b9ea12de20fbbc7332624ff9e3cdc52d3
caaallum <callum_speirs@protonmail.com>
2023-05-12 15:53:27 +0100

    Update smartsplits version

--- lua/plugins/smartsplits.lua
+++ lua/plugins/smartsplits.lua
@@ -1,13 +1,13 @@
 local M = {
   "mrjones2014/smart-splits.nvim",
-  opts = function()
-    return {
-      ignored_buftypes = { "Neo-tree" },
-    }
-  end,
-  config = function(_, opts)
-    require("smart-splits").setup(opts)
-  end
+  event = "VeryLazy",
+  commit = "a2668b59bf3f9b87d47b42e7e8d08200ff0ab3b5"
 }
+
+function M.config()
+  require("smart-splits").setup({
+    ignored_buftypes = { "Neo-tree" }
+  })
+end
 
 return M
