9812e8b69268a4194925ccfff2209e00bf16b66a
Callum <54590992+caaallum@users.noreply.github.com>
2023-05-12 12:57:16 +0000

    Update utils

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -1,105 +1,11 @@
 local M = {}
 
-M.git_colors = {
-    GitAdd = "#A1C281",
-    GitChange = "#74ADEA",
-    GitDelete = "#FE747A",
+M.servers = {
+  "lua_ls",
+  "bashls",
+  "jsonls",
+  "yamlls",
+  "c",
 }
-
-M.lsp_signs = { Error = "✖ ", Warn = "! ", Hint = "󰌶 ", Info = " " }
-
-M.lsp_kinds = {
-    Text = " ",
-    Method = " ",
-    Function = " ",
-    Constructor = " ",
-    Field = " ",
-    Variable = " ",
-    Class = " ",
-    Interface = " ",
-    Module = " ",
-    Property = " ",
-    Unit = " ",
-    Value = " ",
-    Enum = " ",
-    Keyword = " ",
-    Snippet = " ",
-    Color = " ",
-    File = " ",
-    Reference = " ",
-    Folder = " ",
-    EnumMember = " ",
-    Constant = " ",
-    Struct = " ",
-    Event = " ",
-    Operator = " ",
-    TypeParameter = " ",
-    Copilot = " ",
-    Namespace = " ",
-    Package = " ",
-    String = " ",
-    Number = " ",
-    Boolean = " ",
-    Array = " ",
-    Object = " ",
-    Key = " ",
-    Null = " ",
-}
-
-M.mason_packages = {
-    "clangd",
-    "intelephense",
-    "gopls",
-}
-
-M.lsp_servers = {
-    "clangd",
-    "lua_ls",
-    "bashls",
-    "yamlls",
-    "jsonls",
-    "cssls",
-    "html",
-    "gopls"
-}
-
-M.ts_servers = {
-    "bash",
-    "c",
-    "lua",
-    "markdown",
-    "vim",
-    "json",
-    "javascript",
-    "markdown_inline",
-    "yaml",
-    "php"
-}
-
-function M.on_attach(on_attach)
-    vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-            local buffer = args.buf
-            local client = vim.lsp.get_client_by_id(args.data.client_id)
-            on_attach(client, buffer)
-        end,
-    })
-end
-
-function M.warn(msg, notify_opts)
-    vim.notify(msg, vim.log.levels.WARN, notify_opts)
-end
-
-function M.error(msg, notify_opts)
-    vim.notify(msg, vim.log.levels.ERROR, notify_opts)
-end
-
-function M.info(msg, notify_opts)
-    vim.notify(msg, vim.log.levels.INFO, notify_opts)
-end
-
-function M.executable(name)
-  return vim.fn.executable(name) > 0
-end
 
 return M
