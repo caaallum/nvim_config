9bfd1a5a214cbff215fe037cb8aff6ae490ed5a3
caaallum <callum_speirs@protonmail.com>
2023-07-05 21:23:55 +0100

    Removed hardtime update keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -52,10 +52,10 @@ map("n", "|", "<cmd>vsplit<cr>", opts())
 map("n", "\\", "<cmd>split<cr>", opts())
 
 -- Resize splits
-map("n", "<A-h>", function() require("smart-splits").resize_left() end, opts("Resize left"))
-map("n", "<A-j>", function() require("smart-splits").resize_down() end, opts("Resize down"))
-map("n", "<A-k>", function() require("smart-splits").resize_up() end, opts("Resize up"))
-map("n", "<A-l>", function() require("smart-splits").resize_right() end, opts("Resize right"))
+map("n", "<C-A-h>", function() require("smart-splits").resize_left() end, opts("Resize left"))
+map("n", "<C-A-j>", function() require("smart-splits").resize_down() end, opts("Resize down"))
+map("n", "<C-A-k>", function() require("smart-splits").resize_up() end, opts("Resize up"))
+map("n", "<C-A-l>", function() require("smart-splits").resize_right() end, opts("Resize right"))
 
 -- Moving between splits
 map("n", "<C-h>", function() require("smart-splits").move_cursor_left() end, opts("Move left"))
--- lua/plugins/hardtime.lua
+++
@@ -1,7 +0,0 @@
-local M = {
-  "m4xshen/hardtime.nvim",
-  commit = "449e235ea763571ce38396cfd2748817b41fd833",
-  event = "VeryLazy"
-}
-
-return M
